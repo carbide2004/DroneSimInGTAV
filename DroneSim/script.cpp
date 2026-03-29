3cd3c4f84496b1ef52846243ef909ad611409738
Carbide <957091215@qq.com>
2026-03-30 00:12:24 +0800

    feat: 修改采数据变成在3倍STEPSIZE停下使得停下时能够看见目标

--- DroneSim/script.cpp
+++ DroneSim/script.cpp
@@ -925,7 +925,7 @@ static void run_auto_collect(AutoCollectEvent event_type) {
         LOGD("script", std::string("Step ") + std::to_string(step) + ": pos(" + std::to_string(pos.x) + "," + std::to_string(pos.y) + "," + std::to_string(pos.z) + 
              ") target(" + std::to_string(target.x) + "," + std::to_string(target.y) + "," + std::to_string(target.z) + ") dist=" + std::to_string(dist));
         
-        if (dist <= STEPSIZE * 2.0f) { 
+        if (dist <= STEPSIZE * 3.0f) { 
             LOGD("script", std::string("Reached target! Distance ") + std::to_string(dist) + " <= " + std::to_string(STEPSIZE * 2.0f));
             reached = true; 
             break; 
